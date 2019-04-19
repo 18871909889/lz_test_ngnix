a21ab62d9295fd7c2db28962ce165031291c8e45
lizhao <m18871909889@163.com>
2019-04-19 16:21:57 +0800

    Initial commit

---
+++ lz_ngnix_01/src/main/java/lz/ngnix/lz_ngnix_01/LzNgnix01Application.java
@@ -0,0 +1,13 @@
+package lz.ngnix.lz_ngnix_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LzNgnix01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LzNgnix01Application.class, args);
+    }
+
+}
---
+++ lz_ngnix_02/src/main/java/lz/ngnix/lz_ngnix_02/LzNgnix02Application.java
@@ -0,0 +1,13 @@
+package lz.ngnix.lz_ngnix_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LzNgnix02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LzNgnix02Application.class, args);
+    }
+
+}
